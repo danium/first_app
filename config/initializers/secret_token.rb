51bf2314fab031469898a2e5be9da59f13cd3705
danium <irkmetal@mail.ru>
2011-06-16 23:13:39 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sites::Application.config.secret_token = '5b8a2a34e177d100014022e72b06234ff0c0dad286347bb0528a65f6ab37f17ea2808999db39173fc4fa2dc896226238e3ab6faafc18471b6cbecf56e6244d72'
